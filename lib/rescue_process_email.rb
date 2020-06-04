b88dd8aa67ed428cfa65609f7800549be17dca84
Bisho silwal <aayussilwal@gmail.com>
2020-06-04 22:03:20 +0545

    email receiver and rescue processor added

---
+++ lib/rescue_process_email.rb
@@ -0,0 +1,24 @@
+class InvalidReplyUser    < StandardError ; end
+
+class EmailReceive
+  @queue = :incoming_email_queue
+
+  def self.perform(from, to, subject, body)
+    user = User.first
+    if user.nil?
+      raise InvalidReplyUser, "User with email = #{from} is not a member of the app."
+    end
+
+    params = {
+        :body     => body,
+        :to       => to,
+        :subject  => subject,
+        :from     => from
+    }
+
+    message = user.messages.new(params)
+    unless message.save
+      raise RuntimeError, "Unable to save message. Errors: #{message.errors.inspect}"
+    end
+  end
+end
