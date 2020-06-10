6627fe4d449c0d79b5361ca973f7ef23fd493756
Bisho silwal <aayussilwal@gmail.com>
2020-06-10 22:00:27 +0545

    fixed to and from address in message create

--- app/javascript/packs/components/emailMessageCreator.jsx
+++ app/javascript/packs/components/emailMessageCreator.jsx
@@ -5,33 +5,45 @@ import mapDispatchToProps from "react-redux/lib/connect/mapDispatchToProps";
 
 class EmailMessageCreator extends Component {
   state = {
-    from: this.props.from,
+    from: this.props.from || '',
     newMessage: {
       subject: '',
-      body: ''
+      body: '',
+      to: this.props.from || '',
     },
     subjectInvalid: false,
     bodyInvalid: false,
+    toInvalid: false,
     createNewMessage: this.props.createNewMessage
   }
 
   sendMessage = (e) => {
     var that = this;
-    var newMessage = this.state.newMessage;
-    newMessage['to'] = this.state.from;
-    newMessage['from'] = 'test@gmail.com';
+    var { newMessage, createNewMessage, from } = this.state;
+    newMessage['from'] = 'test@gmail.com'; // make sure to change when system generate the mail
+    if(from.length != 0) {
+      newMessage['to'] = from;
+    }
+    if(createNewMessage && newMessage['to'].trim().length == 0) {
+      this.setState({toInvalid: true})
+      e.preventDefault();
+      return
+    }
+
     if(newMessage['subject'].trim().length == 0) {
-      this.setState({subjectInvalid: true})
+      this.setState({ subjectInvalid: true });
       e.preventDefault();
       return;
     }
     this.setState({subjectInvalid: false, bodyInvalid: false})
     if(newMessage['body'].trim().length == 0) {
-      this.setState({bodyInvalid: true})
+      this.setState({ bodyInvalid: true });
       e.preventDefault();
       return;
     }
-    this.setState({bodyInvalid: false})
+
+    this.setState({ bodyInvalid: false });
+
     var token = window.$("meta[name='csrf-token']").attr('content');
     axios.post('/send_email', {
       new_message: newMessage
@@ -42,9 +54,19 @@ class EmailMessageCreator extends Component {
       }
     })
       .then(function (res) {
-        that.setState({newMessage: {subject: '', body: ''}});
+        that.setState({newMessage: {subject: '', body: '', to: ''}});
         toastr.success(res.data.message, res.data.details, {'iconClass': 'toastr-success'})
       })
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if(nextProps.createNewMessage != this.props.createNewMessage) {
+      this.setState({createNewMessage: nextProps.createNewMessage});
+    }
+
+    if(nextProps.from != this.props.from) {
+      this.setState({from: nextProps.from});
+    }
   }
 
   handleChange = (e) => {
@@ -54,10 +76,26 @@ class EmailMessageCreator extends Component {
   }
 
   render() {
-    var { newMessage, subjectInvalid, bodyInvalid, createNewMessage} = this.state;
+    var { newMessage, subjectInvalid, bodyInvalid, createNewMessage, toInvalid } = this.state;
     return(
       <div className='col-12 shadow-lg p-3 pt-0 mb-2 bg-white rounded'>
         <div className='row'>
+          { createNewMessage ?
+            <div className='col-12 p-3'>
+              <div className="form-group row">
+                <label htmlFor="to" className="col-sm-2 col-form-label">
+                  <span className='font-weight-bolder'>To:</span>
+                </label>
+                <div className="col-sm-10">
+                  <input type="text" name='to' value={newMessage.to} ref={(el) => this.mailTo = el} className={"form-control " + `${toInvalid ? 'is-invalid' : ''}` } id="to" placeholder='Enter mail subject' onChange={this.handleChange} required={true}/>
+                  <div className="invalid-feedback">
+                    Please enter email address
+                  </div>
+                </div>
+              </div>
+            </div> :
+            ''
+          }
           <div className='col-12 p-3'>
             <div className="form-group row">
               <label htmlFor="subject" className="col-sm-2 col-form-label">
