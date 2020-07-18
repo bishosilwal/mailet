bf9cdd986b76146abe486a69c8aa9597ba79a471
Bisho silwal <aayussilwal@gmail.com>
2020-07-18 21:40:36 +0545

    added address in session

--- app/controllers/mail_address_controller.rb
+++ app/controllers/mail_address_controller.rb
@@ -1,33 +1,41 @@
 class MailAddressController < ApplicationController
+  after_action :set_address_session
 
   def create
-    mail = MailAddress.random
-    render json: { new_mail: mail, status: :ok }
+    @mail = MailAddress.random
+    render json: { new_mail: @mail, status: :ok }
   end
 
   def destroy
-    mail = MailAddress.random(mail_id_params)
-    render json: { message: 'Mail address deleted', new_mail: mail, details: '', status: :ok }
+    @mail = MailAddress.random(mail_id_params)
+    render json: { message: 'Mail address deleted', new_mail: @mail, details: '', status: :ok }
   end
 
   def change
-    mail = MailAddress.random(mail_id_params)
-    render json: { message: 'Mail address changed', new_mail: mail, status: :ok }
+    @mail = MailAddress.random(mail_id_params)
+    render json: { message: 'Mail address changed', new_mail: @mail, status: :ok }
   end
 
   def custom_address
-    mail = MailAddress.random(**mail_id_params, address: mail_address_params[:mail])
-    if mail.save
+    @mail = MailAddress.random(**mail_id_params, address: mail_address_params[:mail])
+    if @mail.save
       render json: {
         message: 'Mail address created',
-        new_mail: mail
+        new_mail: @mail
       }
     else
-      render json: mail.errors.messages, status: :unprocessable_entity
+      render json: @mail.errors.messages, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_address_session
+    if @mail.persisted?
+      session.delete(:mail_address)
+      session[:mail_address] = @mail.mail
+    end
+  end
 
   def mail_address_params
     params.require(:mail_address).permit(:mail)
