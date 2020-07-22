ec0bf24e03659bd88238cc278a5a412ca20505ed
Bisho silwal <aayussilwal@gmail.com>
2020-07-22 22:00:15 +0545

    added www in action cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'ws://mailet.in/cable'
-  config.action_cable.allowed_request_origins = [ 'http://mailet.in', /http:\/\/mailet.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://mailet.in', /http:\/\/mailet.*/, 'www.mailet.in' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
