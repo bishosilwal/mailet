1081e255f8d11ba7830052c5c6cf7aae57a15377
Bisho silwal <aayussilwal@gmail.com>
2020-07-22 22:37:55 +0545

    removed dynamic added cable url

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -2,5 +2,4 @@
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 
 import { createConsumer } from "@rails/actioncable"
-var cable_url = "ws://" + window.location.host + "/cable"
-export default createConsumer(cable_url)
+export default createConsumer()
