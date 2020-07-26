a2846d18b6b2fd370dbd117e9fbc92b44ebdafbf
Bisho silwal <aayussilwal@gmail.com>
2020-07-26 21:56:58 +0545

    optimized webpacker bundle size by using code splitting

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,31 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const merge = require('merge')
 
-module.exports = environment.toWebpackConfig()
+const path = require('path');
+
+const aliasConfig = {
+    mode: 'production',
+    entry: {
+      application: './app/javascript/packs/application.js',
+      index: './app/javascript/packs/index.jsx',
+      emailContainer: './app/javascript/packs/components/emailContainer.jsx',
+      messageContainer: './app/javascript/packs/components/messageContainer.jsx',
+      customMailAddress: './app/javascript/packs/components/customMailAddress.jsx',
+      customIframe: './app/javascript/packs/components/customIframe.jsx',
+      emailMessageCreator: './app/javascript/packs/components/emailMessageCreator.jsx',
+      message: './app/javascript/packs/components/message.jsx',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve('public', 'packs'),
+    },
+     optimization: {
+       splitChunks: {
+           chunks: 'all',
+           },
+     },
+};
+
+module.exports = merge(environment.toWebpackConfig(), aliasConfig);
