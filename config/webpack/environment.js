8ba23a843ade361a384984317eeedb7b179f2e80
Bisho silwal <aayussilwal@gmail.com>
2020-07-27 22:11:14 +0545

    used preact and removed react

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
+var webpack = require('webpack');
 
+environment.config.resolve.alias = {
+  "react": "preact/compat",
+  "react-dom/test-utils": "preact/test-utils",
+  "react-dom": "preact/compat",
+}
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  }),
+)
+// Enable the default config
 module.exports = environment
