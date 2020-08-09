9916f78d17a2f713f2eb09fa6e9ba049076a3e4d
Bisho silwal <aayussilwal@gmail.com>
2020-08-09 16:28:03 +0545

    splitchunks added

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,37 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require('zlib');
+
+// Enable the default config
+environment.splitChunks()
+environment.config.merge({
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    }
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.jsx$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: '[path].br[query]',
+      algorithm: 'brotliCompress',
+      test: /\.(js|jsx|css|html|svg)$/,
+      compressionOptions: {
+        level: 11,
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
+})
 
 module.exports = environment.toWebpackConfig()
