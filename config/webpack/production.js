85502a4bc30f6d91fad2d25982f4300eb2297ed7
Bisho silwal <aayussilwal@gmail.com>
2020-08-09 16:49:18 +0545

    used merge package

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,35 +3,37 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 const CompressionPlugin = require('compression-webpack-plugin');
 const zlib = require('zlib');
-
+const { merge } = require('webpack-merge')
 // Enable the default config
 environment.splitChunks()
-environment.config.merge({
-  mode: 'production',
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    }
-  },
-  plugins: [
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.jsx$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-    new CompressionPlugin({
-      filename: '[path].br[query]',
-      algorithm: 'brotliCompress',
-      test: /\.(js|jsx|css|html|svg)$/,
-      compressionOptions: {
-        level: 11,
-      },
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-  ],
-})
 
-module.exports = environment.toWebpackConfig()
+module.exports = merge(
+  environment.toWebpackConfig(),
+  {
+    mode: 'production',
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      }
+    },
+    plugins: [
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.jsx$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+      new CompressionPlugin({
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        test: /\.(js|jsx|css|html|svg)$/,
+        compressionOptions: {
+          level: 11,
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
+  }
+)
