949bea508aa03e6e6ba8d129d69f074acc54396f
Bisho silwal <aayussilwal@gmail.com>
2020-12-13 17:02:00 +0545

    all public image moved to img folder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
     '<meta property="og:title" content="Mailet - Temporary Disposable Free Email Address" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://www.mailet.in" />
-    <meta property="og:image" content="https://www.mailet.in/mailet_demo.png" />
+    <meta property="og:image" content="https://www.mailet.in/img/mailet_demo.png" />
     <meta property="og:image:width" content="640" />
     <meta property="og:image:height" content="442" />
     <meta property="og:description" content="Use free temporary disposable random fake email address to send, receive, delete mail with images and richtext. Secure, anonymous, spam free email generator." />
@@ -28,11 +28,11 @@ module ApplicationHelper
   end
 
   def meta_twitter
-    '<meta name="twitter:card" content="/mailet_demo.png" />
+    '<meta name="twitter:card" content="/img/mailet_demo.png" />
     <meta name="twitter:site" content="" />
     <meta name="twitter:title" content="Mailet - Temporary Disposable Free Email Address" />
     <meta name="twitter:description" content="Use free temporary disposable random fake email address to send, receive, delete mail with images and richtext. Secure, anonymous, spam free email generator." />
     <meta name="twitter:creator" content="Bisho silwal" />
-    <meta name="twitter:image:src" content="https://www.mailet.in/mailet_demo.png" />'.html_safe
+    <meta name="twitter:image:src" content="https://www.mailet.in/img/mailet_demo.png" />'.html_safe
   end
 end
