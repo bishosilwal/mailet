b222f7d4eef7e2afda3400818dd09469277c1625
Bisho silwal <aayussilwal@gmail.com>
2020-12-19 22:14:23 +0545

    successfull docker build

--- config/puma.rb
+++ config/puma.rb
@@ -17,40 +17,40 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "production" }
 
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "/var/www/mailet.in/shared"
+# shared_dir = "/var/www/mailet.in/shared"
+# #
+# # # Set up socket location
+# bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+# #
+# # # Specifies the `pidfile` that Puma will use.
+# pidfile ENV.fetch("PIDFILE") { "#{shared_dir}/tmp/pids/server.pid" }
+# #
+# # # Set master PID and state locations
+# state_path "#{shared_dir}/tmp/pids/puma.state"
+# #
+# # # Logging
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 #
-# # Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+# # activate_control_app
+# # Specifies the number of `workers` to boot in clustered mode.
+# # Workers are forked web server processes. If using threads and workers together
+# # the concurrency of the application would be max `threads` * `workers`.
+# # Workers do not work on JRuby or Windows (both of which do not support
+# # processes).
+# #
+# # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 #
-# # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "#{shared_dir}/tmp/pids/server.pid" }
-#
-# # Set master PID and state locations
-state_path "#{shared_dir}/tmp/pids/puma.state"
-#
-# # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# activate_control_app
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
-#
-on_worker_boot do
- require "active_record"
- ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
- ActiveRecord::Base.establish_connection(YAML.load_file("#{shared_dir}/config/database.yml")[rails_env])
-end
-# Allow puma to be restarted by `rails restart` command.
+# # Use the `preload_app!` method when specifying a `workers` number.
+# # This directive tells Puma to first boot the application and load code
+# # before forking the application. This takes advantage of Copy On Write
+# # process behavior so workers use less memory.
+# #
+# # preload_app!
+# #
+# on_worker_boot do
+#  require "active_record"
+#  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#  ActiveRecord::Base.establish_connection(YAML.load_file("#{shared_dir}/config/database.yml")[rails_env])
+# end
+# # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
---
+++ docker-entrypoint/init.sql
@@ -0,0 +1,1 @@
+CREATE USER mailet SUPERUSER;
