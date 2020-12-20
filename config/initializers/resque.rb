a4fafe6e7d4890ffe36cc441793b2cf61e448197
Bisho silwal <aayussilwal@gmail.com>
2020-12-20 17:06:06 +0545

    remove worker service and attached to web service

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,4 @@ rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
 Resque.redis =  Redis.new(url: 'redis://redis:6379/2')
+Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
--- docker-entrypoint/docker-entrypoint.sh
+++ docker-entrypoint/docker-entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+echo "Running web service"
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
-bundle exec rake db:migrate RAILS_ENV=development
-bundle exec rails s -b 0.0.0.0
+
+bundle exec rake db:migrate
+bundle exec rails s -b 0.0.0.0 &
+bundle exec rake resque:work QUEUE='*'
