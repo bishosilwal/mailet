f65827ea800874202a30d44647a53624ec96f4e0
Bisho silwal <aayussilwal@gmail.com>
2020-12-21 22:30:46 +0545

    running webpack dev server in development env

--- docker-entrypoint/docker-entrypoint.sh
+++ docker-entrypoint/docker-entrypoint.sh
@@ -7,6 +7,14 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rake db:migrate
-bundle exec rails s -b 0.0.0.0 &
-bundle exec rake resque:work QUEUE='*'
+if [ "$RAILS_ENV" = "development" ];
+then
+  RAILS_ENV="$RAILS_ENV" bundle exec bin/webpack-dev-server &
+elif [ "$RAILS_ENV" = "production" ];
+then
+  RAILS_ENV="$RAILS_ENV" bundle exec rails assets:precompile
+fi
+
+RAILS_ENV="$RAILS_ENV" bundle exec rake db:migrate
+RAILS_ENV="$RAILS_ENV" bundle exec rails s -b 0.0.0.0 &
+RAILS_ENV="$RAILS_ENV" bundle exec rake resque:work QUEUE='*'
