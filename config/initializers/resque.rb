37d1eaa451377d2c5b0ea92b03e8b62a7461358b
Bisho silwal <aayussilwal@gmail.com>
2021-01-23 22:39:48 +0545

    fix resque connection

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-Resque.redis =  Redis.new(url: 'redis://redis:6379/2')
+Resque.redis =  Redis.new(url: 'redis://localhost:6379/2')
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
