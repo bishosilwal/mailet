b71885a5e0d843528e4fead60b8c03197a1cd3db
Bisho silwal <aayussilwal@gmail.com>
2022-01-04 22:17:53 +0545

    remove summer note reference

--- app/javascript/packs/components/summerNote.jsx
+++ app/javascript/packs/components/summerNote.jsx
@@ -2,7 +2,6 @@
 import $ from 'jquery';
 window.$ = $;
 window.jQuery = $;
-import 'summernote/dist/summernote';
 import 'codemirror/lib/codemirror';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
